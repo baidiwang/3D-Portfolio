ed7d0d44ee4477a366410da73d55013853a1b8c9
baidiwang <baidiwang30@gmail.com>
2025-01-21 23:52:28 -0500

    updated Home and Loader

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,24 @@
-import React from "react";
+import React, { Suspense } from "react";
+import { Canvas } from "@react-three/fiber";
+import Loader from "../components/Loader";
 
 const Home = () => {
-  return <div></div>;
+  return (
+    <section className="w-full h-screen relatetive">
+      <Canvas
+        className="w-full h-screen bg-transparent"
+        camera={{ near: 0.1, far: 1000 }}
+      >
+        <Suspense fallback={<Loader />}>
+          <directionalLight />
+          <ambientLight />
+          <pointLight />
+          <spotLight />
+          <hemisphereLight />
+        </Suspense>
+      </Canvas>
+    </section>
+  );
 };
 
 export default Home;
